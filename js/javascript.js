f3a772ab2a12fe318ea11deb5255ab5978bff5c9
VillarrealOctavio <octavillarreal2016@gmail.com>
2022-02-21 09:05:51 -0300

    Comenzando un nuevo trabajo de Viejas al Hilo

--- js/javascript.js
+++ js/javascript.js
@@ -1,222 +1,244 @@
-/*// Contenedores de productos para incluir estructura HTML en su interior
-const stockDeMates = document.querySelector(".todosMates")
-const stockDeBolsos = document.querySelector(".todosBolsos")
-const stockDeTermos = document.querySelector(".todosTermos")
-const imgAndinista = document.querySelector(".andinista")
+// arrays
+let enElCarrito = [];
+
+
+// CONTENEDORES
+const matesContainer = document.querySelector(".contenedorMates")
+const estuchesContainer = document.querySelector(".contenedorEstuches")
+const latasContainer = document.querySelector(".contenedorLatas")
+const bolsosContainer = document.querySelector(".contenedorBolsos")
 const carritoModal = document.querySelector(".modalCarrito")
 const totalPrice = document.querySelector("#precioTotal")
 
-// Botones para incluir un "evento".
-const btnBolso = document.querySelector("#botonBolsos")
-const btnMate = document.querySelector("#botonMates")
-const btnTermo = document.querySelector("#botonTermos")
-const filterMate = document.querySelector("#filtrarMate")
 
-// Arrays
-let enElCarrito = [];
 
-// Eventos
-btnMate.addEventListener(`click`, ()=>{
-    showProduct(stockMates)
+
+
+// CONTENEDORES PARA EVENTOS
+const btnMates = document.querySelector(".botonMates")
+const btnEstuches = document.querySelector(".botonEstuches")
+const btnLatas = document.querySelector(".botonLatas")
+const btnBolsos = document.querySelector(".botonBolsos")
+const filtradoBolsos = document.querySelector("#filtrarBolsos")
+const filtradoEstuches = document.querySelector("#filtrarEstuches")
+const filtradoLatas = document.querySelector("#filtrarLatas")
+const filtradoMates = document.querySelector("#filtrarMates")
+
+
+
+
+
+// EVENTOS
+// Para bolsos
+btnBolsos.addEventListener(`click`, ()=>{
+    matesContainer.innerHTML=""
+    estuchesContainer.innerHTML=""
+    latasContainer.innerHTML=""
+    mostrarBolsos(stockBolsos)
+})
+filtradoBolsos.addEventListener(`change`, ()=>{
+    if(filtradoBolsos.value=="all"){
+        mostrarBolsos(stockBolsos)
+    }else{
+        mostrarBolsos(stockBolsos.filter(el=>el.clase==filtradoBolsos.value))
+    }
+})
+// Para estuches
+btnEstuches.addEventListener(`click`,()=>{
+    matesContainer.innerHTML=""
+    bolsosContainer.innerHTML=""
+    latasContainer.innerHTML=""
+    mostrarEstuches(stockEstuches)
+})
+filtradoEstuches.addEventListener(`change`, ()=>{
+    if(filtradoEstuches.value=="all"){
+        mostrarEstuches(stockEstuches)
+    }else{
+        mostrarEstuches(stockEstuches.filter(el=>el.clase==filtradoEstuches.value))
+    }
+})
+// Para latas
+btnLatas.addEventListener("click", ()=>{
+    matesContainer.innerHTML=""
+    bolsosContainer.innerHTML=""
+    estuchesContainer.innerHTML=""
+    mostrarLatas(stockLatas)
+})
+filtradoLatas.addEventListener(`change`, ()=>{
+    if(filtradoLatas.value=="all"){
+        mostrarLatas(stockLatas)
+    }else{
+        mostrarLatas(stockLatas.filter(el=>el.clase==filtradoLatas.value))
+    }
+})
+// Para mates
+btnMates.addEventListener(`click`, ()=>{
+    latasContainer.innerHTML=""
+    bolsosContainer.innerHTML=""
+    estuchesContainer.innerHTML=""
+    mostrarMates(stockMates)
+})
+filtradoMates.addEventListener(`change`, ()=>{
+    if(filtradoMates.value=="all"){
+        mostrarMates(stockMates);
+    }else{
+        mostrarMates(stockMates.filter(el=>el.clase==filtradoMates.value))
+    }
 })
 
-/*filterMate.addEventListener(`change`,()=> { //Preguntar este ejercicio al profe
-    console.log(filterMate.value)
-    if (filterMate.value=="todos") {
-        showProduct(stockMates)
-    }else{
-        showProduct(stockMates.filter(el=>el.tipo.includes(filterMate.value)))
-    }
-})*/
 
 
-// Funciones
-/*function showProduct (array) {
-    imgAndinista.innerHTML=""; 
-    let filtrar = document.createElement("Select")
-    filtrar.id=`filtrarMate`
-    filtrar.innerHTML=`          
-                <option value="todos">Todos</option>
-                <option value="azul">Azul</option>
-                <option value="blanco">Blanco</option>
-                <option value="caramelo">Caramelo</option>
-                <option value="carmin">Carmin</option>
-                <option value="celeste">Celeste</option>
-                <option value="fucsia">Fucsia</option>
-                <option value="lila">Lila</option>
-                <option value="rosa">Rosa</option>
-                <option value="terracota">Terracota</option>
-                <option value="turqueza">Turqueza</option>
-                <option value="verde">Verde</option>
-                <option value="violeta">Violeta</option>
-                <option value="marrón">Marrón</option>
-                <option value="naranja">Naranja</option>
-                <option value="negro">Negro</option>
-                <option value="rojo">Rojo</option>
-    `   
-        stockDeMates.prepend(filtrar)
-    array.forEach(el =>{
-        let div = document.createElement("div");
-        div.innerHTML += `
+
+
+
+
+// FUNCIONES
+// Para bolsos
+function mostrarBolsos (array) {
+    bolsosContainer.innerHTML=""
+    array.forEach(el => {
+        const div = document.createElement("div")
+        div.innerHTML+=`
                 <div class="card" style="width: 18rem;">
-                    <img src="${el.img}" class="card-img-top" alt="mate">
+                    <img src="${el.img}" class="card-img-top" alt="imagen">
                     <div class="card-body">
-                        <h5 class="card-title">${el.tipo}</h5>
-                        <p class="card-text">Precio:$${el.precio}</p>
-                        <a id="agregarProducto${el.id}" class="btn btn-primary">Agregar al carrito</a>
+                    <h5 class="card-title">${el.tipo}</h5>
+                    <p class="card-text">Viejas al Hilo</p>
+                    <a href="#" class="btn btn-primary" id="buyBolso${el.id}">Agregar al carrito de compras</a>
                     </div>
                 </div>
         `
-        stockDeMates.appendChild(div);
-        let bntAgregarCarrito = document.querySelector(`#agregarProducto${el.id}`)
-        // console.log(bntAgregarCarrito) No borrar este console.log para tenerlo de guía a la hora de ejecutar.
-        bntAgregarCarrito.addEventListener(`click`,()=> {
-            agregarAlCarritoDeCompras(el.id)
+        bolsosContainer.appendChild(div)
+
+        let btnAgregarAlCarrito = document.querySelector(`#buyBolso${el.id}`)
+        console.log(btnAgregarAlCarrito)
+
+        btnAgregarAlCarrito.addEventListener(`click`, ()=>{
+            console.log(el.id)
+        agregarAlCarrito(el.id)
         })
     });
 }
-// ------------------------------------------------------------
+// Para estuches y bolsas
+function mostrarEstuches (array) {
+    estuchesContainer.innerHTML=""
+    array.forEach(el => {
+        const div = document.createElement("div")
+        div.innerHTML+=`
+                <div class="card" style="width: 18rem;">
+                    <img src="${el.img}" class="card-img-top" alt="imagen">
+                    <div class="card-body">
+                    <h5 class="card-title">${el.tipo}</h5>
+                    <p class="card-text">Viejas al Hilo</p>
+                    <a href="#" class="btn btn-primary" id="buyEstuche${el.id}">Agregar al carrito de compras</a>
+                    </div>
+                </div>
+        `
+        estuchesContainer.appendChild(div)
+        let btnAgregarAlCarrito = document.querySelector(`#buyEstuche${el.id}`)
+        console.log(btnAgregarAlCarrito)
 
+        btnAgregarAlCarrito.addEventListener(`click`, ()=>{
+            console.log(el.id)
+            agregarAlCarrito(el.id)
+        })
+    });
+}
+// Para latas 
+function mostrarLatas (array) {
+    latasContainer.innerHTML=""
+    array.forEach(el => {
+        const div = document.createElement("div")
+        div.innerHTML+=`
+                <div class="card" style="width: 18rem;">
+                    <img src="${el.img}" class="card-img-top" alt="imagen">
+                    <div class="card-body">
+                    <h5 class="card-title">${el.tipo}</h5>
+                    <p class="card-text">Viejas al Hilo</p>
+                    <a href="#" class="btn btn-primary" id="buyLata${el.id}">Agregar al carrito de compras</a>
+                    </div>
+                </div>
+        `
+        latasContainer.appendChild(div)
+        let btnAgregarAlCarrito = document.querySelector(`buyLata${el.id}`)
+        console.log(btnAgregarAlCarrito)
+        btnAgregarAlCarrito.addEventListener(`click`, ()=>{
+            console.log(el.id)
+            agregarAlCarrito(el.id);
+        })
+    });
+}
+// Para mates
+function mostrarMates (array) {
+    matesContainer.innerHTML=""
+    array.forEach(el => {
+        const div = document.createElement("div")
+        div.innerHTML+=`
+                <div class="card" style="width: 18rem;">
+                    <img src="${el.img}" class="card-img-top" alt="imagen">
+                    <div class="card-body">
+                    <h5 class="card-title">${el.tipo}</h5>
+                    <p class="card-text">Viejas al Hilo</p>
+                    <a href="#" class="btn btn-primary" id="buyMate${el.id}">Agregar al carrito de compras</a>
+                    </div>
+                </div>
+        `
+        matesContainer.appendChild(div)
+        let btnAgregarAlCarrito = document.querySelector(`buyMate${el.id}`);
+        console.log(btnAgregarAlCarrito)
+        btnAgregarAlCarrito.addEventListener(`click`, ()=>{
+            console.log(el.id);
+            agregarAlCarrito(el.id);
+        })
+    });
+}
+// Una vez terminada la parte de los productos, es necesario realizar una función que me relacione cada uno de ellos con el carrito
 
-function agregarAlCarritoDeCompras(id) {
- let repeated = enElCarrito.find(item=>item.id==id)
- if(repeated){
-     repeated.cantidad=repeated.cantidad + 1;
- }else{
-    let agregarElProducto = stockMates.find(el=>el.id==id);
-    console.log(agregarElProducto);
+// function agregarAlCarrito
+function agregarAlCarrito (id){
+    let productoRepetido = enElCarrito.find(el=>el.id==id);
+    if(productoRepetido){
+        console.log("El producto está repetido en tu carrito")
+        productoRepetido.cantidad = productoRepetido.cantidad + 1
+        document.querySelector(`cantidad${productoRepetido.id}`).innerHTML= `<p id="cantidad${productoRepetido.id}">Cantidad: ${productoRepetido.cantidad}</p>`
+        actualizarCarritoDeCompras()
+    }else{
+    let agregarElProducto = stockBolsos.find(el=>el.id==id)
+    console.log(agregarElProducto)
     enElCarrito.push(agregarElProducto)
-       carritoModal.innerHTML += `
-               <p>Producto: ${agregarElProducto.tipo}</p>
-               <p>Precio: $${agregarElProducto.precio}</p>
-               <p id="cantidad${agregarElProducto.id}">Cantidad: ${agregarElProducto.cantidad}</p>
-               <button id="btnEliminar${agregarElProducto.id}">Eliminar Producto</button>
+    actualizarCarritoDeCompras();
+
+    let div = document.createElement("div")
+    div.className=`d-flex justify-content-around`
+    div.innerHTML=`
+            <p>${agregarElProducto.tipo}</p>
+            <p>Precio: $${agregarElProducto.precio}</p>
+            <p id="cantidad${agregarElProducto.id}">Cantidad: ${agregarElProducto.cantidad}</p>
+            <button id="eliminar${agregarElProducto.id}">Eliminar</button>
     `
-       const btnEliminar = document.querySelector(`#btnEliminar${agregarElProducto.id}`)
-       btnEliminar.addEventListener(`click`,()=>{
-           console.log(btnEliminar)})
-           console.log(btnEliminar.parentElement)
-   
-       enElCarrito = enElCarrito.filter(el => el.id != agregarElProducto.id)
- }
- localStorage.setItem=(`carritoDeCompra`, JSON.stringify(enElCarrito))
-};
-
-// ------------------------------------------------------------
-function actualizarCarrito (){
-    totalPrice.innerText= enElCarrito.reduce((acc, el)=>acc+(el.cantidad*el.precio), 0);
-};
-
-// -----------------------------------------------------------
-
-function recuperar (){
-    let recuperar = JSON.parse(`carritoDeCompras`)
-};*/
-
-
-
-
-
-
-
-
-
-
-// Contenedores de productos para incluir estructura HTML en su interior
-const stockDeMates = document.querySelector(".todosMates")
-const stockDeBolsos = document.querySelector(".todosBolsos")
-const stockDeTermos = document.querySelector(".todosTermos")
-const imgAndinista = document.querySelector(".andinista")
-const carritoModal = document.querySelector(".modalCarrito")
-const totalPrice = document.querySelector("#precioTotal")
-
-// Botones para incluir un "evento".
-const btnBolso = document.querySelector("#botonBolsos")
-const btnMate = document.querySelector("#botonMates")
-const btnTermo = document.querySelector("#botonTermos")
-const filterMate = document.querySelector("#filtrarMate")
-
-// Arrays
-let enElCarrito = [];
-
-// Eventos
-btnMate.addEventListener(`click`, ()=>{
-    showProduct(stockMates)
-})
-
-filterMate.addEventListener(`change`,()=> { //Preguntar este ejercicio al profe
-    console.log(filterMate.value)
-    if (filterMate.value=="todos") {
-        showProduct(stockMates)
-    }else{
-        showProduct(stockMates.filter(el=> el.color==filterMate.value))
+    carritoModal.appendChild(div)
+    const btnEliminar = document.querySelector(`#eliminar${agregarElProducto.id}`)
+    btnEliminar.addEventListener(`click`, ()=>{
+        console.log(agregarElProducto.id)
+        btnEliminar.parentElement.remove();
+        enElCarrito = enElCarrito.filter(el=>el.id != agregarElProducto.id)
+    actualizarCarritoDeCompras();
+    localStorage.setItem(`carritoDeCompras`, JSON.stringify(enElCarrito));
+})    
     }
-})
-
-
-// Funciones
-function showProduct (array) {
-    imgAndinista.innerHTML=""; 
-    stockDeMates.innerHTML="";
-
-    array.forEach(el =>{
-        let div = document.createElement("div");
-        div.innerHTML += `
-                <div class="card" style="width: 18rem;">
-                    <img src="${el.img}" class="card-img-top" alt="mate">
-                    <div class="card-body">
-                        <h5 class="card-title">${el.tipo}</h5>
-                        <p class="card-text">Precio:$${el.precio}</p>
-                        <a id="agregarProducto${el.id}" class="btn btn-primary">Agregar al carrito</a>
-                    </div>
-                </div>
-        `
-        stockDeMates.appendChild(div);
-        let bntAgregarCarrito = document.querySelector(`#agregarProducto${el.id}`)
-        // console.log(bntAgregarCarrito) No borrar este console.log para tenerlo de guía a la hora de ejecutar.
-        bntAgregarCarrito.addEventListener(`click`,()=> {
-            agregarAlCarritoDeCompras(el.id)
-        })
-    });
+    localStorage.setItem(`carritoDeCompras`, JSON.stringify(enElCarrito));
 }
-// ------------------------------------------------------------
 
+// Función para poder actualizar el carrito cuando sea necesario
+function actualizarCarritoDeCompras (){
+    totalPrice.innerText=enElCarrito.reduce((acc,el)=>acc+(el.cantidad*el.precio), 0)
+}
 
-function agregarAlCarritoDeCompras(id) {
- let repeated = enElCarrito.find(item=>item.id==id)
- /*if(repeated){
-     repeated.cantidad=repeated.cantidad + 1;
- }else{*/
-    let agregarElProductoAlCarrito = stockMates.find(el=>el.id==id);
-    console.log(agregarElProductoAlCarrito);
-    enElCarrito.push(agregarElProductoAlCarrito)
-    actualizarCarrito();
-       carritoModal.innerHTML += `
-               <p>Producto: ${agregarElProductoAlCarrito.tipo}</p>
-               <p>Precio: $${agregarElProductoAlCarrito.precio}</p>
-               <p id="cantidad${agregarElProductoAlCarrito.id}">Cantidad: ${agregarElProductoAlCarrito.cantidad}</p>
-               <button id="btnEliminar${agregarElProductoAlCarrito.id}">Eliminar Producto</button>
-    `
-       const btnEliminar = document.querySelector(`#btnEliminar${agregarElProductoAlCarrito.id}`)
-       btnEliminar.addEventListener(`click`,()=>{
-           console.log(btnEliminar)})
-        //    console.log(btnEliminar.parentElement)
-   
-    //    enElCarrito = enElCarrito.filter(el => el.id != agregarElProductoAlCarrito.id)
- //}
-//  localStorage.setItem=(`carritoDeCompra`, JSON.stringify(enElCarrito))
-};
-
-// ------------------------------------------------------------
-function actualizarCarrito (){
-    totalPrice.innerText= enElCarrito.reduce((acc, producto)=>acc+(producto.cantidad*producto.precio), 0);
-};
-
-// -----------------------------------------------------------
-
-function recuperar (){
-    // let recuperar = JSON.parse(`carritoDeCompras`)
-};
-let mail = document.querySelector(".correo");
-localStorage.setItem("correoElectronico", JSON.stringify(mail))
-let obtener = JSON.parse(localStorage.getItem("correoElectronico"))
+function recuperarData (){
+    let recuperarLs = JSON.parse(localStorage.getItem(`carritoDeCompras`))
+    if(recuperarLs){{
+        recuperarLs.forEach(el=>{
+            enElCarrito(el.id)
+        })
+    }}
+}
